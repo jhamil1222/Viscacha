587a68c4799935b00779d3b3f547a2cb10a74c62
jhamil1222 <50027264+jhamil1222@users.noreply.github.com>
2020-07-22 18:47:37 -0400

    Create inicial.java

---
+++ inicial.java
@@ -0,0 +1,22 @@
+public class inicial{
+public static void main(String []args){
+System.out.println("progreso XD");
+
+
+}
+
+
+}
+class enteros {
+private String [] as;
+public enteros (String [] as){
+this.as=as;
+
+}
+public String setenteros(){
+System.out.println("bucle transportador");
+setenteros();
+}
+
+}
+//nota avanzar pos data espera
